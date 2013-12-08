380c25766d0475b1742f868be27b1abe57742717
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-12-09 00:07:04 +0100

    Added some driver functionality

--- src/Indigo/Pdf/Driver.php
+++ src/Indigo/Pdf/Driver.php
@@ -6,7 +6,15 @@ use Indigo\Pdf\PdfInterface;
 
 abstract class Driver implements PdfInterface
 {
-	private $globalDefaults = array();
+	private $globalDefaults = array(
+		'options' => array(
+            'orientation' => 'P',
+            'page-size'   => 'A4',
+            'unit'        => 'mm',
+            'encoding'    => 'UTF-8',
+		),
+		'page_options' => array(),
+	);
 
 	protected $defaults = array();
 
@@ -14,7 +22,9 @@ abstract class Driver implements PdfInterface
 
 	protected $options = array();
 
-	protected $page_options = array();
+	protected $mapOptions = array();
+
+	protected $pageOptions = array();
 
 	protected $instance = null;
 
@@ -23,30 +33,59 @@ abstract class Driver implements PdfInterface
 		$config = array_merge_recursive($this->globalDefaults, $this->defaults, $config);
 
 		if (array_key_exists('options', $config)) {
-			$this->options = $config['options'];
+			$this->options = $this->map($this->mapOptions, $config['options']);
 			unset($config['options']);
 		}
 
 		if (array_key_exists('page_options', $config)) {
-			$this->page_options = $config['page_options'];
+			$this->pageOptions = $this->map($this->mapOptions, $config['page_options']);
 			unset($config['page_options']);
 		}
 
 		$this->config = $config;
 	}
 
-	public function getConfig($key, $default = null)
+	public function getConfig($key = null, $default = null)
 	{
-		return array_key_exists($key, $this->config) ? $this->config[$key] : $default;
+		return $this->arrGet($this->config, $key, $default);
+	}
+
+	public function getOption($key, $default = null)
+	{
+		return $this->arrGet($this->options, $key, $default);
 	}
 
 	public function getInstance()
 	{
-		return $this->instance;
+		return $this->instance ?: $this;
 	}
 
     protected function isHtml($string)
     {
-        return $string != strip_tags($string);
+        // return $string != strip_tags($string);
+        return preg_match("/<[^<]+>/", $string);
+    }
+
+    protected function arrGet(array $array, $key = null, $default = null)
+    {
+		return is_null($key) ? $array : (array_key_exists($key, $array) ? $array[$key] : $default);
+    }
+
+    protected function map(array $map = array(), array $values = array())
+    {
+    	foreach ($map as $key => $value) {
+    		if (array_key_exists($key, $values)) {
+    			if (is_array($value)) {
+    				$values[$key] = str_replace($value[0], $value[1], $values[$key]);
+    			}
+    			else
+    			{
+    				$values[$value] = $values[$key];
+    				unset($values[$key]);
+    			}
+    		}
+    	}
+
+    	return $values;
     }
 }
